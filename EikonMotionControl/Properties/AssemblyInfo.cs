6c25b18024dc2c337c381e0012b3bf1a31c59946
Xiaoming Wang <xiaoming.wang@thomsonreuters.com>
2014-12-10 12:34:07 +0800

    Initial version

---
+++ EikonMotionControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EikonMotionControl")]
+[assembly: AssemblyDescription("Eikon AppServer App EikonMotionControl")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thomson Reuters")]
+[assembly: AssemblyProduct("Eikon AppServer - EikonMotionControl")]
+[assembly: AssemblyCopyright("Copyright © Thomson Reuters 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: Guid("2995d949-d26f-4d15-9d8a-1d35b2c62d2e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+// AppServer:  Keep the following two version values in sync, and increment each time you publish
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+
---
+++ EikonMotionControl/WebCallableFunctions.cs
@@ -0,0 +1,62 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="$className$.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ThomsonReuters.Eikon.Toolkit.Interfaces;
+
+namespace ThomsonReuters.Eikon.EikonMotionControl
+{
+    // AppServer: match the app name to namespace above, and the class name below in the UiToolkitApp attribute.
+    [UiToolkitApp("ThomsonReuters.Eikon.EikonMotionControl.WebCallableFunctions")]
+    public class WebCallableFunctions
+    {
+        // WebApp: use the following function signature: public string <NameOfFunction>(string query, string body, IAppServerServices services)
+        public string ToUpper(string query, string body, IAppServerServices services)
+        {
+            services.Logger.LogInfo("ToUpper executing in version '{0}' for user '{1}'", GetMyVersion(), services.UserContext.UUID);
+            var builder = new StringBuilder("Query: ");
+            builder.Append(query.ToUpper());
+            builder.Append("\nBody: ");
+            builder.Append(body.ToUpper());
+
+            return builder.ToString();
+        }
+
+        public string ToLower(string query, string body, IAppServerServices services)
+        {
+            services.Logger.LogInfo("ToLower executing in version '{0}' for user '{1}'", GetMyVersion(), services.UserContext.UUID);
+            var builder = new StringBuilder("Query: ");
+            builder.Append(query.ToLower());
+            builder.Append("\nBody: ");
+            builder.Append(body.ToLower());
+            return builder.ToString();
+        }
+
+        public string GetVersion(string query, string body, IAppServerServices services)
+        {
+            var builder = new StringBuilder("Version: ");
+            builder.Append(GetMyVersion());
+            return builder.ToString();
+        }
+
+        public string GetEmail(string query, string body, IAppServerServices services)
+        {
+            var builder = new StringBuilder("Email: ");
+            builder.Append(services.UserContext.EmailAddress);
+            return builder.ToString();
+        }
+
+        private static Version GetMyVersion()
+        {
+            var myAssembly = Assembly.GetExecutingAssembly();
+            return myAssembly.GetName().Version;
+        }
+    }
+}
+
